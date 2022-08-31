90f10849bf211d06351695deac3d517e335bfde5
LyuLumos <3208413453@qq.com>
2022-08-31 16:01:13 +0800

    Passed on 2022-08-31

---
+++ ljy/info.py
@@ -0,0 +1,19 @@
+userAgent = "Mozilla/5.0 (Linux; Android 10; PCT-AL10 Build/HUAWEIPCT-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36"
+studentNumber = "202220083900004"
+pwd = "Ljy62442"
+
+college = "计算机与网络空间安全学院"
+department = "网络空间安全"
+class_ = ""
+phoneNumber = "15635707310"
+studentName = "吕九洋"
+
+curplace = "其他省市"
+province = "山西省"
+city = "临汾市"
+district = "尧都区"
+detailAddress = "卧牛小区"
+latitude = float(111.528401)
+longitude = float(36.08473)
+
+dorm = "校内公寓"
