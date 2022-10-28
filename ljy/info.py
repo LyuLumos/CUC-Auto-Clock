73478bf3fbd9b9c193ff8d8b5a1b2bdfacb3003d
LyuLumos <3208413453@qq.com>
2022-10-28 18:37:40 +0800

    for test

--- ljy/info.py
+++ ljy/info.py
@@ -2,13 +2,11 @@ userid = ""
 userAgent = "Mozilla/5.0 (Linux; Android 10; PCT-AL10 Build/HUAWEIPCT-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36"
 studentNumber = ""
 pwd = ""
-
 college = "计算机与网络空间安全学院"
 department = "网络空间安全"
 class_ = ""
 phoneNumber = "15635707310"
 studentName = "吕九洋"
-
 curplace = "北京"
 province = "北京市"
 city = "北京市"
@@ -16,5 +14,4 @@ district = "朝阳区"
 detailAddress = "中国传媒大学"
 latitude = float()
 longitude = float()
-
 dorm = "中国传媒大学校内8号楼东209"
